f2945aa8fc1a40502de39e744208de1b56ce5196
geniusjoelraj <geniusjoel2005@gmail.com>
2023-04-23 17:41:59 +0530

    Timer complete but have some bugs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.location.href="#pomodoro";
+
 $(".close").click(function() {
   $(".spotify").slideUp();
   $(".close").css("display", "none");
@@ -10,9 +12,47 @@ $(".spotify-logo").click(function() {
   $(".spotify-logo").css("display", "none");
 });
 
-$(".start").click(timer);
+$("#start").click(timerPomodoro);
 
-function timer() {
-  
+let pomodoro = 24;
+let shortBreak = 5;
+let longBreak = 10;
+let tab = "#pomodoro";
+let btn = "#pomo-btn"
+let time = 24;
+
+$("#pomo-btn").click(function() {
+  time = 24;
+  tab = "#pomodoro";
+});
+
+$("#short-btn").click(function() {
+  time = 4;
+  tab = "#short-break";
+});
+
+$("#long-btn").click(function() {
+  time = 9;
+  tab = "#long-break";
+});
+
+
+function timerPomodoro() {
+  let i = 59  ;
+  setInterval(function() {
+    if (time < 10){
+      time = "0"+time;
+    }
+    $(tab).text(time+":00");
+    time--; 
+    i = 59;
+  },60000);
+  setInterval(function() {
+    if (i < 10){
+      i = "0"+i;
+    }
+    $(tab).text(time+":"+i);
+    i--;
+  },1000); 
 }
 
